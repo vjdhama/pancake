ed2c7a39d61fdefba8dec324efb7f33cb876f3f1
vjdhama <vjdhama26@gmail.com>
2013-09-05 14:48:16 +0530

    Basic Rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pancake::Application.config.secret_key_base = '814898124055df92d632b27c6d86771afa5d07ab71e5ccd610c980d614bd55f6909283d0594d60e8d78fbecf0114aaa9a4d2f2950cc850097f3d8ba860b22e92'
