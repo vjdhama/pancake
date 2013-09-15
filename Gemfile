cf33aeec4d10cbbb9afa2a2c7f3e475fc728d16e
pkrawat1 <pankajrawat19sept@gmail.com>
2013-09-15 09:58:19 +0530

    update topbar

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'rails', '4.0.0'
 gem 'sqlite3'
 gem 'haml'
 gem 'zurb-foundation'
-
+gem 'devise'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pancake::Application.routes.draw do
+  devise_for :models
   get "pancake/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
