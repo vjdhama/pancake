329477411f0f522c911a29d8d317974d21a0ba9d
pkrawat1 <pankajrawat19sept@gmail.com>
2013-09-19 23:42:30 +0530

    override bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pancake::Application.routes.draw do
   devise_for :models
   get "pancake/index"
+  resources 'pancake'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
